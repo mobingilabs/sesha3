3abd484783005ccb0f23144b35f1f8e6139d2cee
flowerinthenight <chew.esmero@gmail.com>
2017-09-04 18:13:49 +0900

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Version 4: "+time.Now().String())
+	fmt.Fprintf(w, "Version 5: "+time.Now().String())
 }
 
 func main() {
