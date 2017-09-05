4faf815c16ef9f80bae6a603b9d70fdbd25a1f88
flowerinthenight <chew.esmero@gmail.com>
2017-09-05 16:54:02 +0900

    Initial merge from settyd (#9)
    * Merged from settyd develop.
    
    * Add missing files.
    
    * Add dev param.
    
    * Add cli helper.
    
    * token action add
    
    * Fix build error.
    
    * Rename. (#8)
    
    * Initial merge.

---
+++ helper.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/spf13/cobra"
+
+func flag(cmd *cobra.Command, f string) string {
+	s := cmd.Flag(f).DefValue
+	if cmd.Flag(f).Changed {
+		s = cmd.Flag(f).Value.String()
+	}
+
+	return s
+}
+
+func GetCliStringFlag(cmd *cobra.Command, f string) string {
+	return flag(cmd, f)
+}
