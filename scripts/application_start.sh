8e03d067ba41fc1fb2f5ce78aeb63486d5a3004e
flowerinthenight <chew.esmero@mobingi.com>
2017-09-07 16:40:24 +0900

    Use supervisor for restart.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cd /home/ubuntu/sesha3/
-./sesha3 --syslog > /dev/null 2> /dev/null < /dev/null &
+ln -sf /home/ubuntu/sesha3/supervisor.conf /etc/supervisor/conf.d/sesha3.conf
+supervisorctl start sesha3 > /dev/null 2> /dev/null < /dev/null
+# cd /home/ubuntu/sesha3/
+# ./sesha3 --syslog > /dev/null 2> /dev/null < /dev/null &
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-killall -v sesha3 > /dev/null 2> /dev/null < /dev/null
-exit 0
+# killall -v sesha3 > /dev/null 2> /dev/null < /dev/null
+supervisorctl stop sesha3 > /dev/null 2> /dev/null < /dev/null
+# exit 0
