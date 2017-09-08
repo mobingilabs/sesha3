d3999b1b74b1711d3217471c94e528b607264918
flowerinthenight <chew.esmero@mobingi.com>
2017-09-08 15:09:09 +0900

    Change endpoint.

--- serve.go
+++ serve.go
@@ -169,7 +169,7 @@ func serve(cmd *cobra.Command) {
 	port := GetCliStringFlag(cmd, "port")
 	router := mux.NewRouter()
 	router.HandleFunc("/token", token.Settoken).Methods(http.MethodGet)
-	router.HandleFunc("/json", tty).Methods(http.MethodGet)
+	router.HandleFunc("/ttyurl", tty).Methods(http.MethodGet)
 	router.HandleFunc("/version", version).Methods(http.MethodGet)
 	err := http.ListenAndServeTLS(":"+port, certfolder+"/fullchain.pem", certfolder+"/privkey.pem", router)
 	d.ErrorExit(err, 1)
