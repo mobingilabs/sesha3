a1e73ed62f17b7b70b484a10b0dba3daf2a528cd
Yuto Kimura <biostudy28@gmail.com>
2017-09-12 11:35:01 +0900

    fix build err

--- context.go
+++ context.go
@@ -52,7 +52,7 @@ func (c *context) Start() (ret string, err error) {
 		ssh := "/usr/bin/ssh -oStrictHostKeyChecking=no -i " + os.TempDir() + "/user/" + c.StackId + ".pem " + c.User + "@" + c.Ip
 		shell := "grep " + c.User + " /etc/passwd | cut -d: -f7"
 		dshellb, _ := exec.Command("bash", "-c", ssh+" -t "+shell).Output()
-		d.Info("default:" + dshellb)
+		d.Info(dshellb)
 		defaultshell := strings.TrimSpace(string(dshellb))
 		d.Info("default:" + defaultshell)
 		timeout := c.Timeout
