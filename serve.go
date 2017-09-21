4974fac25d2af0275a79a6893dce51fabd55ab6d
flowerinthenight <chew.esmero@mobingi.com>
2017-09-21 15:37:30 +0900

    Version up.

--- serve.go
+++ serve.go
@@ -127,7 +127,7 @@ func describeSessions(w http.ResponseWriter, req *http.Request) {
 }
 
 func version(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte(`{"version":"v0.0.10-beta"}`))
+	w.Write([]byte(`{"version":"v0.0.11-beta"}`))
 }
 
 func redirect(w http.ResponseWriter, req *http.Request) {
