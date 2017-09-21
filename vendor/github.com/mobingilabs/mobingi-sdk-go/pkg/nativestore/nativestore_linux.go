5d18125210fa072c2bb84fa2605d1f90fc568c79
flowerinthenight <chew.esmero@gmail.com>
2017-09-21 08:46:38 +0000

    Prune vendors.

--- vendor/github.com/mobingilabs/mobingi-sdk-go/pkg/nativestore/nativestore_linux.go
+++
@@ -1,9 +0,0 @@
-package nativestore
-
-import "github.com/docker/docker-credential-helpers/pass"
-
-//passphrase of gpg2 is empty
-//pass setting (below commands)
-//$ gpg2 --gen-key 
-//$ pass init [gpgID]
-var ns = pass.Pass{}
