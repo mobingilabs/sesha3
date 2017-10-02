1809da6ece58d27fad6bdc6022f400ba5145d8ab
Yuto Kimura <biostudy28@gmail.com>
2017-10-02 13:46:46 +0900

    test

--- serve.go
+++ serve.go
@@ -175,8 +175,8 @@ func serve(cmd *cobra.Command) {
 	// redirect every http request to https
 	// go http.ListenAndServe(":80", http.HandlerFunc(redirect))
 	// everything else will be https i
-	//d.Info("debug:errcheck start")
-	//go errcheck()
+	d.Info("debug:errcheck start")
+	go errcheck()
 	//check notification flags
 	d.Info("serve:start")
 	d.Info("credprof:", credprof)
