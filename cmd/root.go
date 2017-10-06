8851af250c5cf427b17a05b8d28e5cd9efe784f1
Yuto Kimura <biostudy28@gmail.com>
2017-10-06 09:26:50 +0900

    Get instance id from ec2 itself:test

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,8 @@ import (
 	"github.com/mobingilabs/mobingi-sdk-go/pkg/debug"
 	"github.com/mobingilabs/sesha3/pkg/params"
 	"github.com/spf13/cobra"
+	"io/ioutil"
+	"net/http"
 )
 
 var rootCmd = &cobra.Command{
@@ -20,13 +22,21 @@ func Execute() {
 	}
 }
 
+func GetEc2Id() string {
+	url := "http://169.254.169.254/latest/meta-data/instance-id"
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	return string(byteArray)
+}
+
 func init() {
 	rootCmd.Flags().SortFlags = false
 	rootCmd.PersistentFlags().StringVar(&params.Environment, "env", "dev", "values: dev, test, prod")
 	rootCmd.PersistentFlags().BoolVar(&params.UseSyslog, "syslog", false, "set log output to syslog")
 	rootCmd.PersistentFlags().StringArray("notify-endpoints", []string{"slack"}, "values: slack")
 	rootCmd.PersistentFlags().StringVar(&params.Region, "aws-region", "ap-northeast-1", "aws region")
-	rootCmd.PersistentFlags().StringVar(&params.Ec2Id, "ec2-id", "i-0d6ff50d6caef8ffa", "ec2 server instance id")
+	params.Ec2Id = GetEc2Id()
 	rootCmd.PersistentFlags().StringVar(&params.CredProfile, "cred-profile", "sesha3", "aws credenfile profile name")
 	rootCmd.SetHelpCommand(&cobra.Command{
 		Use:   "help",
