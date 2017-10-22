2088de98a0112d6f3337c27b17d6c90040623270
Yuto Kimura <biostudy28@gmail.com>
2017-10-22 14:28:00 +0900

    http get test

---
+++ pkg/exec/exec.go
@@ -0,0 +1,29 @@
+package exec
+
+import (
+	"encoding/json"
+	"github.com/mobingilabs/mobingi-sdk-go/mobingi/sesha3"
+	d "github.com/mobingilabs/mobingi-sdk-go/pkg/debug"
+	"github.com/mobingilabs/sesha3/pkg/notify"
+	"io/ioutil"
+	"net/http"
+)
+
+func Run(w http.ResponseWriter, r *http.Request) {
+	var getdata map[string]interface{}
+
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.Write(sesha3.NewSimpleError(err).Marshal())
+		notify.HookPost(err)
+		return
+	}
+	d.Info("body:", string(body))
+	err = json.Unmarshal(body, &getdata)
+	if err != nil {
+		w.Write(sesha3.NewSimpleError(err).Marshal())
+		notify.HookPost(err)
+		return
+	}
+}
