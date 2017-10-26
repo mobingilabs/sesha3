ba761a137d8e25b36b5f85dfaa4d56e800477a53
Yuto Kimura <biostudy28@gmail.com>
2017-10-27 08:51:58 +0900

    debug

--- cmd/serve.go
+++ cmd/serve.go
@@ -361,11 +361,12 @@ func execScript(w http.ResponseWriter, r *http.Request) {
 	//
 
 	// pemfile download for ssh
-	pemurls := getdata["pem"].([]sesha3.StackPem)
+	pemurls := getdata["pem"].([]interface{})
 	d.Info(pemurls)
 	_ = targets
-	//	for _, pemurl := range pemurls {
-	//		d.Info("rawurl:", pemurl)
+	for _, pemurl := range pemurls {
+		d.Info("rawurl:", pemurl.(sesha3.StackPem))
+	}
 	//		resp, err := http.Get(fmt.Sprintf("%v", pemurl))
 	//		if err != nil {
 	//			w.Write(sesha3.NewSimpleError(err).Marshal())
