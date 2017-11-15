b26036319711eb0c8d7401aecfe78a06b080fd2b
flowerinthenight <chew.esmero@gmail.com>
2017-11-15 03:37:28 +0000

    Debug.

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sudo su -
-/usr/local/bin/supervisorctl stop sesha3 > /dev/null 2> /dev/null < /dev/null
+# sudo su -
+# /usr/local/bin/supervisorctl stop sesha3 > /dev/null 2> /dev/null < /dev/null
+echo hello
