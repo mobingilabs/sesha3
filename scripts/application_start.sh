ba387d9b7826d1201523ee5830edfece4ce7077b
flowerinthenight <chew.esmero@gmail.com>
2017-11-16 04:21:26 +0000

    Reorg supervisor start.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-# sudo su -
+# append our [program] to supervisor if not present
+grep -q -F '[program:sesha3]' foo.bar || \
+    echo '' >> foo.bar && \
+    echo '[program:sesha3]' >> foo.bar && \
+    echo 'command=/home/ec2-user/sesha3/sesha3 serve --syslog' >> foo.bar && \
+    echo 'directory=/home/ec2-user/sesha3' >> foo.bar && \
+    echo 'autostart=true' >> foo.bar && \
+    echo 'autorestart=true' >> foo.bar && \
+    echo 'stderr_logfile=syslog' >> foo.bar && \
+    echo 'stdout_logfile=syslog' >> foo.bar
+
+/usr/local/bin/supervisorctl reread > /dev/null 2> /dev/null < /dev/null
+/usr/local/bin/supervisorctl update > /dev/null 2> /dev/null < /dev/null
 /usr/local/bin/supervisorctl start sesha3 > /dev/null 2> /dev/null < /dev/null
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# sudo su -
 /usr/local/bin/supervisorctl stop sesha3 > /dev/null 2> /dev/null < /dev/null
