cf8acd2aa82aa748dacbdf43a1aab754e9ec0e45
flowerinthenight <chew.esmero@gmail.com>
2017-11-21 07:37:25 +0000

    Back to close port.

--- pkg/session/session.go
+++ pkg/session/session.go
@@ -58,14 +58,11 @@ func (s *Session) Start() (string, error) {
 
 	// port closer function
 	fnClosePort := func() {
-		d.Info("simulate port close")
-		/*
-			err := ec2req.ClosePort()
-			if err != nil {
-				notify.HookPost(err)
-				s.error(errors.Wrap(err, "close port failed"))
-			}
-		*/
+		err := ec2req.ClosePort()
+		if err != nil {
+			notify.HookPost(err)
+			s.error(errors.Wrap(err, "close port failed"))
+		}
 	}
 
 	go func() {
