bde4a35e20b47f3ee69e5ada16e937717a41d36c
flowerinthenight <chew.esmero@gmail.com>
2017-11-22 07:23:12 +0000

    Add master in cmdline logic.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # append our [program:sesha3] to supervisor if not present
-grep 'sesha3' /etc/supervisord.conf || echo -e '\n[program:sesha3]\ncommand=/home/ec2-user/sesha3/sesha3 serve --syslog --rundev\ndirectory=/home/ec2-user/sesha3\nautostart=true\nautorestart=true\nstderr_logfile=syslog\nstdout_logfile=syslog' >> /etc/supervisord.conf
+grep -q 'sesha3' /etc/supervisord.conf || echo -e '\n[program:sesha3]\ncommand=/home/ec2-user/sesha3/sesha3 serve --syslog --rundev\ndirectory=/home/ec2-user/sesha3\nautostart=true\nautorestart=true\nstderr_logfile=syslog\nstdout_logfile=syslog' >> /etc/supervisord.conf
 
 # replace command+args in supervisord.conf based on final args
 sed -i -e '/^.*serve.*$/{r /home/ec2-user/sesha3/cmdline' -e 'd}' /etc/supervisord.conf &>> /home/ec2-user/codedeploy.log
