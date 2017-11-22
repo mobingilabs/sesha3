19dac33dbb4527e71a445a64cd5034290b8b3576
flowerinthenight <chew.esmero@gmail.com>
2017-11-22 05:32:01 +0000

    Check accum conf.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# append our [program] to supervisor if not present
-grep 'sesha3' /etc/supervisord.conf || \
-    echo -en '\n' >> /etc/supervisord.conf && \
-    echo '[program:sesha3]' >> /etc/supervisord.conf && \
-    echo 'command=/home/ec2-user/sesha3/sesha3 serve --syslog --rundev' >> /etc/supervisord.conf && \
-    echo 'directory=/home/ec2-user/sesha3' >> /etc/supervisord.conf && \
-    echo 'autostart=true' >> /etc/supervisord.conf && \
-    echo 'autorestart=true' >> /etc/supervisord.conf && \
-    echo 'stderr_logfile=syslog' >> /etc/supervisord.conf && \
-    echo 'stdout_logfile=syslog' >> /etc/supervisord.conf
+# append our [program:sesha3] to supervisor if not present
+grep 'sesha3' /etc/supervisord.conf || echo -e '\n[program:sesha3]\ncommand=/home/ec2-user/sesha3/sesha3 serve --syslog --rundev\ndirectory=/home/ec2-user/sesha3\nautostart=true\nautorestart=true\nstderr_logfile=syslog\nstdout_logfile=syslog' >> /etc/supervisord.conf
 
+# reload supervisor
 /usr/local/bin/supervisorctl reread &>> /home/ec2-user/codedeploy.log
 /usr/local/bin/supervisorctl update &>> /home/ec2-user/codedeploy.log
 /usr/local/bin/supervisorctl start sesha3 &>> /home/ec2-user/codedeploy.log
